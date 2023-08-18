090fdc8139d8dfc99acda7514d5ea91092e7ebc8
sis-shen <141546659+sis-shen@users.noreply.github.com>
2023-08-18 21:48:47 +0800

    08-18

---
+++ 08-18/08-18/08-18.cpp
@@ -0,0 +1,37 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//
+//	pa++;
+//	printf("%s\n", *pa);
+//
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//	printf("%s\n", **++cpp);
+//	printf("%s\n", *-- * ++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//
+//
+//	return 0;
+//}
+
+int main()
+{
+	char* p = "abcdef";
+
+	return 0;
+}
--- 0816/0816/08-16.cpp
+++ 0816/0816/08-16.cpp
@@ -2,12 +2,31 @@
 
 #include <stdio.h>
 
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 };
+//	int* ptr = (int*)(&a + 1);
+//	printf("%d %d", *(a + 1), *(ptr - 1));
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[4] = { 1,2,3,4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x, %x", ptr1[-1], *ptr2);
+//
+//	return 0;
+//}
+
 int main()
 {
-	int a[5] = { 1,2,3,4,5 };
-	int* ptr = (int*)(&a + 1);
-	printf("%d %d", *(a + 1), *(ptr - 1));
+	int a[] = {1,2,3,4};
+	
+	printf("%p , %p", &(a[0]), &(a[1]));
+
 
 	return 0;
 }
-
